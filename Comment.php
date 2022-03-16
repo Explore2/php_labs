65b2b3a98c35a60cf4a65855867af0db60b2503c
Explore2 <darkshift911@gmail.com>
2022-03-16 16:58:42 +0300

    lab1 almost done

---
+++ Comment.php
@@ -0,0 +1,27 @@
+<?php
+require_once(__DIR__.'/User.php');
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validation;
+class Comment
+{
+    private $user;
+    public $text;
+ function __construct($user, $text){
+     $validator = Validation::createValidator();
+
+    $this->user = $user;
+     $violations = $validator->validate($text, [
+         new NotBlank(),
+     ]);
+     if(count($violations) == 0){
+        $this->text = $text;
+    }else{
+         foreach ($violations as $violation){
+             echo $violation->getMessage().'<br>';
+         }
+    }
+ }
+ function getUser(){
+     return $this->user;
+ }
+}
--- hello.php
+++ hello.php
@@ -1,2 +1,13 @@
 <?php
+    require_once(__DIR__.'/User.php');
+    require_once(__DIR__.'/Comment.php');
+    require_once(__DIR__.'/vendor/autoload.php');
+    $user1 = new User(0, "name1", "user1@yandex.ru", "1234dfad");
+    $user2 = new User(1, "name2", "user2@yandex.ru", "1234dfad");
+    $user3 = new User(2, "name3", "user3@yandex.ru", "1234dfad");
+    $comment1 = new Comment($user1, "text from user1");
+    $comment2 = new Comment($user2, "text from user2");
+    $comment3 = new Comment($user3, "text from user3");
+    
+    echo $user1->getCreationDate()->format("Y-m-d H:i:s");
 	echo "Hello from PHP";
